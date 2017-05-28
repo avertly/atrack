88f7e26dda7fb8c4bc21886ec0305d7fcf61092a
Avery O <ave@ave.zone>
2017-05-28 19:40:36 +0300

    Fix #1, rework some parts, add comments

--- atrack.sh
+++ atrack.sh
@@ -16,11 +16,11 @@ influxdb_ip='http://something:8086'
 influxdb_username='username'
 influxdb_password='password'
 influxdb_database='database'
-influxdb_report_name='report'
+influxdb_report_name=$(hostname) # Switch this to a set string if you want, but this ensures that you don't pollute the data.
 
 # End of configuration part
 
-# Based on https://github.com/prurigro/darkcloud-tmuxconfig/blob/master/bwrate -- MIT licensed:
+# Network track -- Based on https://github.com/prurigro/darkcloud-tmuxconfig/blob/master/bwrate -- MIT licensed:
 
 [[ ! -d "/sys/class/net/$network_device" ]] && {
     printf '%s\n' "No such device: $network_device"
@@ -42,24 +42,19 @@ printf '%s\n' "Storage Use: ${used_space}"
 
 total_ram_bytes=$(free | grep Mem | awk '{print $2}')
 used_ram_bytes=$(free | grep Mem | awk '{print $3}')
-printf '%s\n' "Memory Use: ${used_ram_bytes} out of ${total_ram_bytes} bytes."
+ram_percentage=$((100*$used_ram_bytes/$total_ram_bytes))
+if [ "$used_ram_bytes" -eq 0 ]; then # Check if it is set or not
+   ram_percentage=0
+fi
+printf '%s\n' "Memory Use: ${used_ram_bytes} out of ${total_ram_bytes} bytes (${ram_percentage}%)."
 
 total_swap_bytes=$(free | grep Swap | awk '{print $2}')
 used_swap_bytes=$(free | grep Swap | awk '{print $3}')
-printf '%s\n' "Swap Use: ${used_swap_bytes} out of ${total_swap_bytes} bytes."
-
-ram_percentage=$((100*$used_ram_bytes/$total_ram_bytes))
 swap_percentage=$((100*$used_swap_bytes/$total_swap_bytes))
-
-if [ "$used_ram_bytes" -eq 0 ]; then
-   ram_percentage=0
-fi
-
-if [ "$used_swap_bytes" -eq 0 ]; then
+if [ "$used_swap_bytes" -eq 0 ]; then # Check if it is set or not
    swap_percentage=0
 fi
-
-printf '%s\n' "RAM: ${ram_percentage}%, Swap: ${swap_percentage}%."
+printf '%s\n' "Swap Use: ${used_swap_bytes} out of ${total_swap_bytes} bytes (${swap_percentage}%)."
 
 # CPU
 # sysstat is needed for this!
@@ -69,15 +64,13 @@ printf '%s\n' "CPU Use: ${cpu_used}%"
 
 cpu_load_average=$(uptime  | grep -oP '(?<=average:).*' | awk '{print $1}')
 
-# Uptime
-
-system_uptime=$(uptime | awk '{print $1}')
-printf '%s\n' "Uptime: ${system_uptime}"
+# Back to Network stuff
 
 sleep_time=$((60 - $(date +%S) ))
 printf '%s\n' "Sleeping for ${sleep_time} seconds"
 sleep $sleep_time &&
 
+# Sleeping so we can get a minute worth of data to calculate average Network use, and so that we can only report back every minute
 
 br2=$(cat /sys/class/net/"$network_device"/statistics/rx_bytes)
 bt2=$(cat /sys/class/net/"$network_device"/statistics/tx_bytes)
@@ -86,5 +79,7 @@ u_speed=$(( ( ( bt2 - bt1 ) / sleep_time ) / 1024 ))
 d_speed=$(( ( ( br2 - br1 ) / sleep_time ) / 1024 ))
 
 printf '%s\n' "Average Network Speeds: Download: ${d_speed}KB/s, Upload: ${u_speed}KB/s"
+
+# Send to InfluxDB:
 
 curl -i -XPOST "$influxdb_ip/write?u=$influxdb_username&p=$influxdb_password&db=$influxdb_database" --data-binary "$influxdb_report_name storage_usage=${used_space//%},cpu_load=${cpu_load_average//,},cpu_percentage=$cpu_used,ram_percentage=$ram_percentage,swap_percentage=$swap_percentage,download=$d_speed,upload=$u_speed"
